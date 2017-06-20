a4ef04e2903927efc24f46c990d8d333cf522d11
Ahmed Alaskalany <ahmed@alaskalany.com>
2017-06-20 11:48:42 +0200

    Created Test scene, and GameSettings.cs MonoBehaviour script.

---
+++ Assets/Scripts/GrandStrategy/GameSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GrandStrategy
+{
+    public class GameSettings : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
